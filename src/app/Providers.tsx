c418f9bdd545a365c1212d789c887b18643d713c
Muzaffar Ali <muzafark834@gmail.com>
2025-01-11 01:06:35 +0500

    first commit

---
+++ src/app/Providers.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import React from 'react'
+import { Store } from './(addtocartfun)/Redux/store';
+import { Provider } from 'react-redux';
+
+function Providers({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+  return (
+    <div>
+       <Provider store={Store}>
+        {children}
+        </Provider>
+    </div>
+  )
+}
+
+export default Providers
